c02a661887b87b444d0050cfdcdda7c6b70acb7f
Hjy02 <ckach13@gmail.com>
2022-07-12 21:49:04 +0900

    Create Hello.java

---
+++ Java-Eclipse/Java-Easy/Chapter2/Hello.java
@@ -0,0 +1,13 @@
+package Chapter2;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		System.out.println("¾È³ç!");
+		System.out.println("¾È³ç" + "!");
+		
+		String hello = "¾È³ç";
+		System.out.println(hello);
+	}
+
+}
